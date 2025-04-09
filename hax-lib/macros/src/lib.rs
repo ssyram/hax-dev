c60174259662c1788caf4f2d4cffa8daf91f3f03
Maxime Buyse <maxime@cryspen.com>
2025-04-09 14:22:59 +0200

    Put macro_metavar_expr_concat feature under hax cfg.

--- hax-lib/macros/src/lib.rs
+++ hax-lib/macros/src/lib.rs
@@ -1,7 +1,7 @@
 // Proc-macros must "reside in the root of the crate": whence the use
 // of `std::include!` instead of proper module declaration.
 
-#![feature(macro_metavar_expr_concat)]
+#![cfg_attr(hax, feature(macro_metavar_expr_concat))]
 
 #[cfg(hax)]
 std::include!("implementation.rs");
