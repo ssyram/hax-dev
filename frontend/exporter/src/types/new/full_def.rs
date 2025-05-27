a10089a1c8a03315363e45d787fed204d0e544ea
Nadrieril <nadrieril+git@gmail.com>
2025-05-27 12:02:03 +0200

    Translate drop glue shim

--- frontend/exporter/src/types/new/full_def.rs
+++ frontend/exporter/src/types/new/full_def.rs
@@ -188,18 +188,27 @@ pub enum FullDefKind<Body> {
         param_env: ParamEnv,
         #[value(s.base().tcx.adt_def(s.owner_id()).sinto(s))]
         def: AdtDef,
+        /// MIR body of the builtin `drop` impl.
+        #[value(drop_glue_shim(s.base().tcx, s.owner_id()).and_then(|body| Body::from_mir(s, body)))]
+        drop_glue: Option<Body>,
     },
     Union {
         #[value(get_param_env(s, s.owner_id()))]
         param_env: ParamEnv,
         #[value(s.base().tcx.adt_def(s.owner_id()).sinto(s))]
         def: AdtDef,
+        /// MIR body of the builtin `drop` impl.
+        #[value(drop_glue_shim(s.base().tcx, s.owner_id()).and_then(|body| Body::from_mir(s, body)))]
+        drop_glue: Option<Body>,
     },
     Enum {
         #[value(get_param_env(s, s.owner_id()))]
         param_env: ParamEnv,
         #[value(s.base().tcx.adt_def(s.owner_id()).sinto(s))]
         def: AdtDef,
+        /// MIR body of the builtin `drop` impl.
+        #[value(drop_glue_shim(s.base().tcx, s.owner_id()).and_then(|body| Body::from_mir(s, body)))]
+        drop_glue: Option<Body>,
     },
     /// Type alias: `type Foo = Bar;`
     TyAlias {
@@ -359,6 +368,9 @@ pub enum FullDefKind<Body> {
             opt_body.and_then(|body| Body::from_mir(s, body))
         })]
         once_shim: Option<Body>,
+        /// MIR body of the builtin `drop` impl.
+        #[value(drop_glue_shim(s.base().tcx, s.owner_id()).and_then(|body| Body::from_mir(s, body)))]
+        drop_glue: Option<Body>,
     },
 
     // Constants
@@ -944,6 +956,20 @@ fn closure_once_shim<'tcx>(
     };
     let mir = tcx.instance_mir(instance.def).clone();
     let mir = ty::EarlyBinder::bind(mir).instantiate(tcx, instance.args);
+    Some(mir)
+}
+
+#[cfg(feature = "rustc")]
+fn drop_glue_shim<'tcx>(tcx: ty::TyCtxt<'tcx>, def_id: RDefId) -> Option<mir::Body<'tcx>> {
+    let drop_in_place = tcx.require_lang_item(rustc_hir::LangItem::DropInPlace, None);
+    if !tcx.generics_of(def_id).is_empty() {
+        // Hack: layout code panics if it can't fully normalize types, which can happen e.g. with a
+        // trait associated type. For now we only translate the glue for monomorphic types.
+        return None;
+    }
+    let ty = tcx.type_of(def_id).instantiate_identity();
+    let instance_kind = ty::InstanceKind::DropGlue(drop_in_place, Some(ty));
+    let mir = tcx.instance_mir(instance_kind).clone();
     Some(mir)
 }
 
