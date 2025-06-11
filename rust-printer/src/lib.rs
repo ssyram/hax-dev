a58eb877a35ddb81810c041f8e9c4930b2581cc7
Lucas Franceschino <lucas@cryspen.com>
2025-06-11 17:48:33 +0200

    fix(rengine/tests): lints

--- rust-printer/src/lib.rs
+++ rust-printer/src/lib.rs
@@ -1,10 +1,12 @@
 //! The Rust engine of hax.
 
 #![feature(rustc_private)]
-#![warn(broken_intra_doc_links)]
-#![warn(missing_docs)]
-#![warn(unused_qualifications)]
-#![warn(unused_crate_dependencies)]
+#![warn(
+    rustdoc::broken_intra_doc_links,
+    missing_docs,
+    unused_qualifications,
+    unused_crate_dependencies
+)]
 
 pub mod ast;
 pub mod symbol;
