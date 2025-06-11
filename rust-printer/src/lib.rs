e9f235a7ba3f3970513720ed447cb625aa20e11d
Lucas Franceschino <lucas@cryspen.com>
2025-06-11 12:56:41 +0200

    feat(rengine): enable various lints

--- rust-printer/src/lib.rs
+++ rust-printer/src/lib.rs
@@ -1,4 +1,10 @@
+//! The Rust engine of hax.
+
 #![feature(rustc_private)]
+#![warn(broken_intra_doc_links)]
+#![warn(missing_docs)]
+#![warn(unused_qualifications)]
+#![warn(unused_crate_dependencies)]
 
 pub mod ast;
 pub mod symbol;
