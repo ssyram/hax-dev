bafc2cc50139eb6f1ae763034ad39e9a0341545e
Lucas Franceschino <lucas@cryspen.com>
2025-07-30 17:40:56 +0200

    feat(rengine): intro. `Span::dummy` and `GlobalId::def_id` methods

--- rust-engine/src/ast/identifiers.rs
+++ rust-engine/src/ast/identifiers.rs
@@ -102,6 +102,13 @@ pub mod global_id {
             }
         }
 
+        /// Extract the raw `DefId` from a `GlobalId`.
+        /// This should never be used for name printing.
+        pub fn def_id(&self) -> DefId {
+            let (GlobalId::Concrete(concrete_id) | GlobalId::Projector(concrete_id)) = self;
+            concrete_id.def_id.def_id.clone()
+        }
+
         /// Raw printing of identifier separated by underscore. Used for testing
         pub fn to_debug_string(&self) -> String {
             match self {
