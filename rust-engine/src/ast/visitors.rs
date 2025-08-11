38b98e67f1f45a089d0c6b879b08443d5180f399
Lucas Franceschino <lucas@cryspen.com>
2025-08-11 08:18:57 +0200

    Apply suggestion from @keks
    Co-authored-by: Jan Winkelmann <146678+keks@users.noreply.github.com>

--- rust-engine/src/ast/visitors.rs
+++ rust-engine/src/ast/visitors.rs
@@ -33,7 +33,7 @@ pub mod wrappers {
 
     /// A visitor wrapper that tracks span while visiting the AST. Whenever an
     /// AST node that carries a span is visited, using this wrapper, the ambient
-    /// span is mutated and accisble via the `HasSpan` trait.
+    /// span is mutated and accessible via the `HasSpan` trait.
     pub struct SpanWrapper<'a, V>(pub &'a mut V);
 
     impl<'a, V: HasSpan> SpanWrapper<'a, V> {
