cd66ec33bad0ed59af7ae54413c07645ddc238bc
Lucas Franceschino <lucas@cryspen.com>
2025-08-13 17:41:53 +0200

    feat(rengine): enhance and fix `Print` trait

--- rust-engine/src/printer.rs
+++ rust-engine/src/printer.rs
@@ -1,6 +1,8 @@
 //! This modules provides types and helper for the printers of hax.
 
 mod allocator;
+use std::ops::Deref;
+
 pub use allocator::Allocator;
 
 use crate::ast;
@@ -19,6 +21,18 @@ pub trait Resugaring: for<'a> dyn_compatible::AstVisitorMut<'a> {
 pub trait Printer: Sized {
     /// A list of resugaring phases.
     fn resugaring_phases() -> Vec<Box<dyn Resugaring>>;
+    /// The name of the printer
+    const NAME: &str;
+}
+
+/// Implement `Printer` for `Allocator<P>` when `P` implements `Printer`.
+/// This is just a convenience implementation proxying the underlying `Printer` implementation for `P`.
+impl<P: Printer> Printer for Allocator<P> {
+    fn resugaring_phases() -> Vec<Box<dyn Resugaring>> {
+        P::resugaring_phases()
+    }
+
+    const NAME: &str = P::NAME;
 }
 
 /// Placeholder type for sourcemaps.
@@ -27,24 +41,22 @@ pub struct SourceMap;
 /// Helper trait to print AST fragments.
 pub trait Print<T>: Printer {
     /// Print a fragment using a backend.
-    fn print(self, fragment: &mut T) -> Option<(String, SourceMap)>;
+    fn print(self, fragment: T) -> (String, SourceMap);
 }
 
-impl<P: Printer, T: ast::visitors::AstVisitorMut + ast::visitors::AstVisitableInfallible> Print<T>
-    for P
+impl<P: Printer, T> Print<T> for P
 where
     for<'a, 'b> &'b T: Pretty<'a, Allocator<Self>, ast::span::Span>,
     // The following node is equivalent to "T is an AST node"
     for<'a> dyn Resugaring: dyn_compatible::AstVisitableMut<'a, T>,
 {
-    fn print(self, fragment: &mut T) -> Option<(String, SourceMap)> {
+    fn print(self, mut fragment: T) -> (String, SourceMap) {
         for mut reguaring_phase in Self::resugaring_phases() {
-            reguaring_phase.visit(fragment)
+            reguaring_phase.visit(&mut fragment)
         }
         let allocator = Allocator::new(self);
-        let doc = fragment.pretty(&allocator).into_doc();
-        let mut mem = Vec::new();
-        doc.render(80, &mut mem).ok()?;
-        Some((str::from_utf8(&mem).ok()?.to_string(), SourceMap))
+        let doc_builder: pretty::BoxDoc<'_, ast::span::Span> =
+            fragment.pretty(&allocator).into_doc();
+        (doc_builder.deref().pretty(80).to_string(), SourceMap)
     }
 }
