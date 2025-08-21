7d7b31525d59e95f23a2d906be536a59c9412634
Lucas Franceschino <lucas@cryspen.com>
2025-08-21 17:51:51 +0200

    doc(rengine): phase

--- rust-engine/src/phase.rs
+++ rust-engine/src/phase.rs
@@ -5,5 +5,6 @@ use crate::ast::Item;
 /// Placeholder trait for phases.
 pub trait Phase {
     /// Apply the phase on items.
+    /// A phase may transform an item into zero, one or more items.
     fn apply(&self, items: &mut Vec<Item>);
 }
