aa072e627360bc51f6b010dcefa1ed281dfca1e6
Clément Blaudeau <clement.blaudeau@gmail.com>
2025-08-30 22:48:42 +0200

    proof-lib/lean Add more tuple types

--- hax-lib/proof-libs/lean/Hax/Lib.lean
+++ hax-lib/proof-libs/lean/Hax/Lib.lean
@@ -728,22 +728,81 @@ section Tuples
 namespace Rust_primitives.Hax
 
 structure Tuple0 where
-  Tuple0 ::
 
 structure Tuple1 (α0: Type) where
-  Tuple1 ::
   _0 : α0
 
 structure Tuple2 (α0 α1: Type) where
-  Tuple2 ::
   _0 : α0
   _1 : α1
 
 structure Tuple3 (α0 α1 α2: Type) where
-  Tuple3 ::
   _0 : α0
   _1 : α1
   _2 : α2
+
+structure Tuple4 (α0 α1 α2 α3 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+
+structure Tuple5 (α0 α1 α2 α3 α4 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+
+structure Tuple6 (α0 α1 α2 α3 α4 α5 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+  _5 : α5
+
+structure Tuple7 (α0 α1 α2 α3 α4 α5 α6 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+  _5 : α5
+  _6 : α6
+
+structure Tuple8 (α0 α1 α2 α3 α4 α5 α6 α7 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+  _5 : α5
+  _6 : α6
+  _7 : α7
+
+structure Tuple9 (α0 α1 α2 α3 α4 α5 α6 α7 α8 α9 : Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+  _5 : α5
+  _6 : α6
+  _7 : α7
+  _8 : α8
+
+structure Tuple10 (α0 α1 α2 α3 α4 α5 α6 α7 α8 α9: Type) where
+  _0 : α0
+  _1 : α1
+  _2 : α2
+  _3 : α3
+  _4 : α4
+  _5 : α5
+  _6 : α6
+  _7 : α7
+  _8 : α8
+  _9 : α9
 
 end Rust_primitives.Hax
 end Tuples
